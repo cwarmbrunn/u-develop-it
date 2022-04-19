8e5f7a0f6d72c3b7bef773fdf04d84bed46e422f
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-04-19 13:00:49 -0400

    added queries

--- server.js
+++ server.js
@@ -26,9 +26,35 @@ const db = mysql.createConnection(
   console.log("Connected to the election database ✅")
 );
 
-db.query(`SELECT * FROM candidates`, (err, rows) => {
-  console.log(rows);
-});
+// GET a single (1) candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+// Create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+// VALUES (?,?,?,?)`;
+
+// const params = [1, "Ronald", "Firbank", 1];
+
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+// Delete a Candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//   console.log(rows);
+// });
 // THIS MUST BE LAST OR IT WILL OVERRIDE OTHER METHODS
 // Default response for any other request (Not Found)
 app.use((req, res) => {
